e9cb76a602015ee654d1ec0a07fdf305d9cca24a
RISMOO <maurice.sider29@gmail.com>
2020-08-24 15:45:20 +0200

    First Commit

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Routing\UrlGenerator;
+
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -11,12 +11,9 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function boot (UrlGenerator $url )
+    public function register ( )
     {
-        if (env('REDIRECT_HTTPS')){
 
-            $url->formatScheme('https');
-        }
     }
 
     /**
@@ -24,12 +21,10 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function register()
+    public function boot()
     {
-        if (env('REDIRECT_HTTPS')){
 
-$this->app['request']->server->set('HTTPS',true);
 
-    }
+
 }
 }
